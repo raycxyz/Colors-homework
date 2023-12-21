b80b2e56c02fa1eb01727d82eb5303610f003803
raycxyz <raywkh14@gmail.com>
2023-12-20 20:21:32 -0800

    Adding Colors Xcode project

---
+++ Colors/Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Raymond Ho on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
