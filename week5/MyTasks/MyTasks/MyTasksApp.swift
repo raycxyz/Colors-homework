f327ee66c167af5b8c810353107e52ce13d6069e
Raymond Ho <raywkh14@gmail.com>
2024-01-28 19:47:35 -0800

    project setup

---
+++ week5/MyTasks/MyTasks/MyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTasksApp.swift
+//  MyTasks
+//
+//  Created by Raymond Ho on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct MyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
